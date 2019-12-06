129afbe96b3d4ffd3d97ea2ac26d0a415eac08d7
JesseGonzalez024 <jessegonzalez024@gmail.com>
2019-12-06 18:18:09 -0500

    methods update, hash fix

--- lib/person_test/cli.rb
+++ lib/person_test/cli.rb
@@ -2,16 +2,15 @@
 
 class PersonTest
     
-    
     def call
         greeting
         sleep(1)
         tester
-        # results method
+        results
     end
     
     def greeting
-        #A llows name/ collects name and greets user by name
+
         puts "Welcome to The Office personality test!"
         sleep(2)
         puts "Which Office character do you most resemble?"
@@ -26,58 +25,74 @@ Your test is now starting #{@user_name}..."
     end
     
     def tester
-        character = {
-            michael: [name: "Michael Scott", counter: 0, digit: "7642"],
-            dwight: [name: "Dwight Schrute", counter: 0, digit: "7643"],
-            jim: [name: "Jim Halpert", counter: 0, digit: "7644"],
-            kelly: [name: "Kelly Kapoor", counter: 0, digit: "7645"],
-            meredith: [name: "Meredith Palmer", counter: 0, digit: "7647"],
-            angela: [name: "Angela Martin", counter: 0, digit: "7648"]
+        @character = {
+            michael: {name: "Michael Scott", counter: 0, digit: "7642"},
+            dwight: {name: "Dwight Schrute", counter: 0, digit: "7643"},
+            jim: {name: "Jim Halpert", counter: 0, digit: "7644"},
+            kelly: {name: "Kelly Kapoor", counter: 0, digit: "7645"},
+            meredith: {name: "Meredith Palmer", counter: 0, digit: "7647"},
+            angela: {name: "Angela Martin", counter: 0, digit: "7648"}
         }
         
         puts "Please type in the number of your answer"
         puts 'You may also type "exit" at any time'
         puts " "
-        
+    
         Scraper.new 
         Question.all.each_with_index do |question, index|
             puts " "
             puts "#{index += 1 }.  #{question.title}"
             puts " "
-            answers  = question.answers.each_with_index do |answer, index|
+            answers = question.answers.each_with_index do |answer, index|
                 puts "#{index + 1}. #{answer.title}"
-                        end   
-                        input = gets.chomp
-                        result = input.to_i - 1
-                            
-                            #binding.pry
-                            if answers[result].id == "7642"
-                                character[:michael][:counter] + 1
+            end   
+            input = gets.chomp
+                        
+            if input == "exit"
+                puts "Goodbye"
+            end
+                        
+            result = input.to_i - 1
+                        
+            if result == (0..3)
+                if answers[result].id == "7642"
+                    @character[:michael][:counter] += 1
 
-                            elsif answers[result].id == "7643"
-                                character[:dwight][:counter] + 1
+                elsif answers[result].id == "7643"
+                    @character[:dwight][:counter] += 1
 
-                            elsif answers[result].id == "7644"
-                                character[:jim][:counter] + 1
-                            
-                            elsif answers[result].id == "7645"
-                                character[:kelly][:counter] + 1
+                elsif answers[result].id == "7644"
+                    @character[:jim][:counter] += 1
+                        
+                elsif answers[result].id == "7645"
+                    @character[:kelly][:counter] += 1
 
-                            elsif answers[result].id == "7646"
-                                character[:meredith][:counter] + 1
+                elsif answers[result].id == "7646"
+                    @character[:meredith][:counter] += 1
 
-                            elsif answers[result].id == "7647"
-                                character[:angela][:counter] + 1
-                            end
-                    end
+                elsif answers[result].id == "7647"
+                    @character[:angela][:counter] += 1
+                end
+            else
+                puts "Please input a number 1-4"
+            end   
+        end
     end
     
     
     
     def results
-        # calculates majority of answers
-        # compares answers to chacter profiles
-        # return majority character resemblance
+        array = []
+        @character.each do |x, v|
+          array << v[:counter]
+       end
+       winner = array.sort.last
+        @character.select do |k, v|
+            if v[:counter] == winner
+                puts "The Office character you most resemble is: #{v[:name]}"
+            end 
+        end
+        
     end
 
 
